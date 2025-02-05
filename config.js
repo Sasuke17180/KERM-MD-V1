42d5316b7a9d461833d32895dfa34207f8bd8ff2
Sasuke17180 <mesaliens3@gmail.com>
2025-02-05 13:42:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CQ40BbSA#LQ_MK4Sk1ldRPl4DExoB7Awx2J-Wtm8COM3eizp4SvU",
+SESSION_ID: process.env.SESSION_ID || "7FxW1DBQ#Mh2rinckvf1LkCnm5ZAAojAYNctrlQ09qEM8B54bSEk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/ezcxvc.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/py4ols.mp4",
 ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR IM ONLINE I'M KERM-MD-V1 WHATSAPP BOT\n\n> 𝖡𝖸 𝖪𝖦𝖳𝖤𝖢𝖧",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
@@ -20,9 +20,9 @@ FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 FAKE_TYPING: process.env.FAKE_TYPING || "true",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "𝐗-𝐒𝐀𝐒𝐔𝐊𝐄",
+BOT_NAME: process.env.BOT_NAME || "𝑩𝒓𝒊👸🏼🧿💕¡!",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
